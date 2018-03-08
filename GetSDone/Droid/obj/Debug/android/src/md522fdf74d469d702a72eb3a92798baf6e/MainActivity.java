335c92c25dbd3060d6f5f4d30c7604f185e880e7
Leonard Thomas <t-lethom@microsoft.com>
2018-03-08 21:32:06 +0100

    Added the project to the repo

---
+++ GetSDone/Droid/obj/Debug/android/src/md522fdf74d469d702a72eb3a92798baf6e/MainActivity.java
@@ -0,0 +1,47 @@
+package md522fdf74d469d702a72eb3a92798baf6e;
+
+
+public class MainActivity
+	extends md5b60ffeb829f638581ab2bb9b1a7f4f3f.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("GetSDone.Droid.MainActivity, GetSDone.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("GetSDone.Droid.MainActivity, GetSDone.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ GetSDone/GetSDone/GetSDonePage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace GetSDone
+{
+    public partial class GetSDonePage : ContentPage
+    {
+        void OnFocus(object sender, Xamarin.Forms.FocusEventArgs e)
+        {
+            var entry = sender as Entry;
+
+            if (entry == Todo1)
+            {
+                entry.BackgroundColor = _colors[0];
+            }
+            if (entry == Todo2)
+            {
+                entry.BackgroundColor = _colors[1];
+            }
+            if (entry == Todo3)
+            {
+                entry.BackgroundColor = _colors[2];
+            }
+            if (entry == Todo4)
+            {
+                entry.BackgroundColor = _colors[3];
+            }
+            if (entry == Todo5)
+            {
+                entry.BackgroundColor = _colors[4];
+            }
+            if (entry == Todo6)
+            {
+                entry.BackgroundColor = _colors[5];
+            }
+        }
+
+
+        private List<Color> _colors = new List<Color>();
+
+        public GetSDonePage()
+        {
+            InitializeComponent();
+
+            Todo1.BackgroundColor = Color.LightGray;
+            Todo2.BackgroundColor = Color.LightGray;
+            Todo3.BackgroundColor = Color.LightGray;
+            Todo4.BackgroundColor = Color.LightGray;
+            Todo5.BackgroundColor = Color.LightGray;
+            Todo6.BackgroundColor = Color.LightGray;
+
+            _colors.Add(Color.Yellow);
+            _colors.Add(Color.MistyRose);
+            _colors.Add(Color.Goldenrod);
+            _colors.Add(Color.CornflowerBlue);
+            _colors.Add(Color.Green);
+            _colors.Add(Color.IndianRed);
+            _colors.Add(Color.Orange);
+            _colors.Add(Color.MediumPurple);
+
+
+        }
+    }
+}
